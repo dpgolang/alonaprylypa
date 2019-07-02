3e70f0df3d021dc34bc223f8b5e1a6307e53543d
alyonaprylypa <alyonka130198@gmail.com>
2019-07-02 19:10:19 +0300

    gofmt

--- Task5/main/check_test.go
+++ Task5/main/check_test.go
@@ -1,32 +1,34 @@
 package main
-import(
+
+import (
 	"testing"
 )
-func TestInitVal(t *testing.T){
+
+func TestInitVal(t *testing.T) {
 	var str string
-	str=InitVal(125,true,"","","")
-	if str!="сто двадцать пять "{
-		t.Error("Expected сто двадцать пять ,got ",str)
+	str = InitVal(125, true, "", "", "")
+	if str != "сто двадцать пять " {
+		t.Error("Expected сто двадцать пять ,got ", str)
 	}
 }
 func TestCorrectEnd(t *testing.T) {
 	var str string
-	str = CorrectEnd(1,"тысяча ","тысячи ","тысяч ")
-	if str!="тысяча "{
-		t.Error("Expected тысяча got: ",str)
+	str = CorrectEnd(1, "тысяча ", "тысячи ", "тысяч ")
+	if str != "тысяча " {
+		t.Error("Expected тысяча got: ", str)
 	}
 }
 func TestReverse(t *testing.T) {
 	var str string
-	str = Reverse("one","two ")
-	if str!="two one"{
-		t.Error("Expected two one, got ",str)
+	str = Reverse("one", "two ")
+	if str != "two one" {
+		t.Error("Expected two one, got ", str)
 	}
 }
 func TestStringValue(t *testing.T) {
 	var str string
 	str = StringValue(1245)
-	if str!="одна тысяча двести сорок пять "{
-		t.Error("Expected одна тысяча двести сорок пять ,got ",str)
+	if str != "одна тысяча двести сорок пять " {
+		t.Error("Expected одна тысяча двести сорок пять ,got ", str)
 	}
 }
--- Task5/main/main.go
+++ Task5/main/main.go
@@ -4,71 +4,84 @@ import (
 	"fmt"
 	"strconv"
 )
-//const maxNumber=1000000000000
-var hunds = [10]string{"","сто ","двести ","триста ","четыреста ","пятьсот ","шестьсот ","семьсот ","восемьсот ","девятьсот "}
-var tens = [10]string{"","десять ","двадцать ","тридцать ","сорок ","пятьдесят ","шестьдесят ","семьдесят ","восемьдесят ","девяносто "}
-func InitVal(val int, male bool, one, two, five string)(str string){
-	frac20:=[20]string{"","один ","два ","три ","четыре ","пять ","шесть ","семь ","восемь ","девять ","десять ","одиннадцать ","двенадцать ","тринадцать ","четырнадцать ","пятнадцать ","шестнадцать ","семнадцать ","восемьнадцать ","девятнадцать "}
-	var num=val%1000
-	if num==0{return ""}
-	if num<0{return ""}
-	if(!male){
-		frac20[1]="одна "
-		frac20[2]="две "
-	}
-	str=hunds[num/100]
 
-	if num%100<20{
-		str+=frac20[num%100]
-	}else{
-		str+=tens[num%100/10]
-		str+=frac20[num%10]
+//const maxNumber=1000000000000
+var hunds = [10]string{"", "сто ", "двести ", "триста ", "четыреста ", "пятьсот ", "шестьсот ", "семьсот ", "восемьсот ", "девятьсот "}
+var tens = [10]string{"", "десять ", "двадцать ", "тридцать ", "сорок ", "пятьдесят ", "шестьдесят ", "семьдесят ", "восемьдесят ", "девяносто "}
+
+func InitVal(val int, male bool, one, two, five string) (str string) {
+	frac20 := [20]string{"", "один ", "два ", "три ", "четыре ", "пять ", "шесть ", "семь ", "восемь ", "девять ", "десять ", "одиннадцать ", "двенадцать ", "тринадцать ", "четырнадцать ", "пятнадцать ", "шестнадцать ", "семнадцать ", "восемьнадцать ", "девятнадцать "}
+	var num = val % 1000
+	if num == 0 {
+		return ""
 	}
-	str+=CorrectEnd(num,one,two,five)
+	if num < 0 {
+		return ""
+	}
+	if !male {
+		frac20[1] = "одна "
+		frac20[2] = "две "
+	}
+	str = hunds[num/100]
+
+	if num%100 < 20 {
+		str += frac20[num%100]
+	} else {
+		str += tens[num%100/10]
+		str += frac20[num%10]
+	}
+	str += CorrectEnd(num, one, two, five)
 	return str
 }
-func CorrectEnd(val int,one,two,five string)string{
-	t:=val%100
-	if t>20{
-		t = val%10
-	}else{
-		t = val%20
+func CorrectEnd(val int, one, two, five string) string {
+	t := val % 100
+	if t > 20 {
+		t = val % 10
+	} else {
+		t = val % 20
 	}
-	switch t{
-	case 1: return one
-	case 2,3,4: return two
-	default: return five
+	switch t {
+	case 1:
+		return one
+	case 2, 3, 4:
+		return two
+	default:
+		return five
 	}
 	return ""
 }
-func Reverse(s,s1 string) (result string) {
-	return s1+s
+func Reverse(s, s1 string) (result string) {
+	return s1 + s
 }
-func StringValue(value int)(str string){
-	minus:=false
-	if value<0{
+func StringValue(value int) (str string) {
+	minus := false
+	if value < 0 {
 		value = -value
 		minus = true
 	}
-	n:=value
-	if n==0{str+="ноль"}
-	if n%1000!=0{str+=InitVal(n,true,"","","")}
-	n/=1000
-	newStr:=Reverse(str,InitVal(n,false,"тысяча ","тысячи ","тысяч "))
-	n/=1000
-	newStr=Reverse(newStr,InitVal(n,true,"миллион ","миллиона ","миллионов "))
-	n/=1000
-	newStr=Reverse(newStr,InitVal(n,true,"миллиард ","миллиарда ","миллиардов "))
-	n/= 1000;
-	newStr=Reverse(newStr,InitVal(n, true, "триллион ", "триллиона ", "триллионов "));
-	n /= 1000;
-	newStr=Reverse(newStr,InitVal(n, true, "триллиард ", "триллиарда ", "триллиардов "));
+	n := value
+	if n == 0 {
+		str += "ноль"
+	}
+	if n%1000 != 0 {
+		str += InitVal(n, true, "", "", "")
+	}
+	n /= 1000
+	newStr := Reverse(str, InitVal(n, false, "тысяча ", "тысячи ", "тысяч "))
+	n /= 1000
+	newStr = Reverse(newStr, InitVal(n, true, "миллион ", "миллиона ", "миллионов "))
+	n /= 1000
+	newStr = Reverse(newStr, InitVal(n, true, "миллиард ", "миллиарда ", "миллиардов "))
+	n /= 1000
+	newStr = Reverse(newStr, InitVal(n, true, "триллион ", "триллиона ", "триллионов "))
+	n /= 1000
+	newStr = Reverse(newStr, InitVal(n, true, "триллиард ", "триллиарда ", "триллиардов "))
 	if minus {
-		newStr=Reverse(newStr,"минус ")
+		newStr = Reverse(newStr, "минус ")
 	}
 	return newStr
 }
-func getAnswer()int{
+func getAnswer() int {
 	var (
 		n   int
 		str string
@@ -76,17 +89,18 @@ func getAnswer()int{
 	)
 	fmt.Println("Please enter the value of n")
 	for {
-		fmt.Scan(&str)
+		fmt.Scanf("%s", &str)
 		n, err = strconv.Atoi(str)
 		if err != nil {
-			fmt.Print("Please, write the correct value\n")
+			err = fmt.Errorf("Please, write the correct value, %s\n", err)
+			fmt.Print(err)
 		} else {
 			break
 		}
 	}
 	return n
 }
-func main(){
-	n:=getAnswer()
+func main() {
+	n := getAnswer()
 	fmt.Print(StringValue(n))
 }
